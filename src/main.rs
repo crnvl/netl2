c37fe758e9454d9b569fa8b2681e3e20942d9480
angelsflyinhell <angelsflyinhell@namespace.media>
2023-07-16 05:51:45 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,29 @@ use crate::logic::{ast::parse, interpeter::interpret};
 mod logic;
 
 fn main() {
-    let script = std::fs::read_to_string("./examples/spec_test.nl").unwrap();
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Error: No file path provided");
+        return;
+    }
+
+    let path = &args[1];
+
+    let script = match std::fs::read_to_string(path) {
+        Ok(script) => script,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
 
     let tokens = logic::tokenizer::tokenize(&script);
-
-    let parsed = parse(tokens).unwrap();
-
-    println!("{:#?}", parsed);
-
+    let parsed = match parse(tokens) {
+        Ok(parsed) => parsed,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
     interpret(parsed);
 }
